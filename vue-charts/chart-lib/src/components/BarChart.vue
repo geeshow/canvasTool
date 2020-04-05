4d53159a0134b3dcf3d35d259081e548506629aa
geeshow <kute80@naver.com>
2020-04-05 18:07:41 +0900

    vue chart

---
+++ vue-charts/chart-lib/src/components/BarChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <canvas @click="$emit('refreshChart')" ref="barChart" id="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  props: ["propsdata"],
+  mounted() {
+    new this.$_Chart(this.$refs.barChart.getContext("2d"), {
+      type: "bar",
+      data: this.propsdata,
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        },
+      }
+    });
+  }
+};
+</script>
+
+<style scoped>
+</style>
